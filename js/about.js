d55702ae244ca8e53b5e71d19006b0f71d097960
YaohanXu <xuyaohan99@gmail.com>
2024-12-20 10:06:26 +0000

    Final add function to show resume

--- js/about.js
+++ js/about.js
@@ -1,3 +1,18 @@
+// Add a click event to show the resume when the button is clicked
+const button = document.querySelector('.view-resume-button');
+const popupOverlay = document.getElementById('popup-overlay');
+
+button.addEventListener('click', () => {
+  popupOverlay.style.display = 'flex';
+});
+
+// Hide the resume when clicking outside the image
+popupOverlay.addEventListener('click', (event) => {
+  if (event.target === popupOverlay) {
+    popupOverlay.style.display = 'none';
+  }
+});
+
 // Display the page after it is loaded
 window.addEventListener('load', () => {
   document.body.classList.add('loaded'); // Add the loaded class to the body
